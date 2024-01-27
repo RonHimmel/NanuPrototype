ee36d673f91e835a3f91f83dfc4b5443108c9485
Rajpreet0 <Rajpreet.Singh@gmx.de>
2024-01-27 16:33:02 +0100

    Bug Fix on RegEx in Email Validation

--- src/main/java/nanuv01/FormValidation.java
+++ src/main/java/nanuv01/FormValidation.java
@@ -43,7 +43,7 @@ public class FormValidation {
     
     public boolean checkEmailCorrectness(String email){
         
-        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\\\.)+[a-zA-Z]{2,7}$";
+        String emailRegex = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
         
         Pattern pattern = Pattern.compile(emailRegex);
         
